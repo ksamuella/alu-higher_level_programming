9905ca010a3f5c87679060c57e06b9c2e637daa0
ksamuella <your@email.com>
2026-06-26 23:53:13 +0200

    Fix 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import sys
-args = sys.argv[1:]
-n = len(args)
-if n == 0:
-    print("0 arguments.")
-elif n == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(n))
-for i, arg in enumerate(args):
-    print("{}: {}".format(i + 1, arg))
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    n = len(args)
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(n))
+    for i, arg in enumerate(args):
+        print("{}: {}".format(i + 1, arg))

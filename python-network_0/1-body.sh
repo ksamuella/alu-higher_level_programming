e9fda9c3e487c6c8fcdd28b6d94e10a750c3cf18
ksamuella <k.samuella.alustudent.com>
2026-08-01 07:52:21 -0700

    Add python-network_0 tasks 0-5

---
+++ python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Sends a GET request and displays the body only if status is 200
+curl -s "$1"
---
+++ python-network_0/3-methods.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# Displays all HTTP methods a server accepts, using OPTIONS
+curl -s -X OPTIONS -D - -o /dev/null "$1" | grep -i "^Allow:" | cut -d " " -f2- | tr -d "\r"

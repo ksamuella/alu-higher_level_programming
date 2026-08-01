13b6fa0ce135d7f266e0acb24959af56ab124180
ksamuella <k.samuella.alustudent.com>
2026-08-01 12:39:15 -0700

    Add python-object_relational_mapping tasks 0-14

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines the State class for SQLAlchemy ORM."""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represent a state, linked to the MySQL table states."""
+
+    __tablename__ = "states"
+    id = Column(
+        Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
